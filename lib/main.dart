33e145324528792cd72654d53ac2ce31ac64e4e9
tehnplk <tehnplk@gmail.com>
2020-05-23 22:53:14 +0700

    tehnn

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,14 +27,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-bool _loading;
-List _outputs;
-File _image;
-int count=0;
+  bool _loading;
+  List _outputs;
+  File _image;
+  int count = 0;
 
-void initState() {
+  void initState() {
     super.initState();
-
 
     _loading = true;
     loadModel().then((value) {
@@ -43,33 +41,33 @@ void initState() {
         _loading = false;
       });
     });
-}
+  }
 
-
-
-@override
-void dispose() {
-
+  @override
+  void dispose() {
     super.dispose();
-}
+  }
 
-loadModel() async {
+  loadModel() async {
     await Tflite.loadModel(
       model: "assets/model_unquant.tflite",
       labels: "assets/labels.txt",
     );
-}
-pickImage(bool yes) async {
+  }
+
+  pickImage(bool yes) async {
     count++;
-    var image = await ImagePicker.pickImage(source: yes?ImageSource.gallery:ImageSource.camera);
+    var image = await ImagePicker.pickImage(
+        source: yes ? ImageSource.gallery : ImageSource.camera);
     if (image == null) return null;
     setState(() {
       _loading = true;
-      _image = image; 
+      _image = image;
     });
     classifyImage(image);
   }
-classifyImage(File image) async {
+
+  classifyImage(File image) async {
     var output = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 2,
@@ -83,64 +81,70 @@ classifyImage(File image) async {
       _outputs = output;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text("ตรวจจับหน้ากาก"),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent),
+        appBar: AppBar(
+            title: Text("ตรวจจับหน้ากาก"),
+            centerTitle: true,
+            backgroundColor: Colors.deepOrangeAccent),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             FloatingActionButton(
               heroTag: null,
-              onPressed:()=> pickImage(true),
+              onPressed: () => pickImage(true),
               child: Icon(Icons.image),
             ),
             FloatingActionButton(
               heroTag: null,
-              onPressed:()=> pickImage(false),
+              onPressed: () => pickImage(false),
               child: Icon(Icons.camera),
             ),
           ],
         ),
-      
-      body: _loading 
-          ? Container(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            )
-          : Container(
-              color: Colors.grey[50],
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _image == null ? Container() : Image.file(_image,fit: BoxFit.contain,height:MediaQuery.of(context).size.height*0.6),
-
-                  _outputs != null
-                      ? Column(
-                        children: <Widget>[
-                          Text(
-                              _outputs[0]["label"]=='0 mask'?"ใส่หน้ากาก":"ไม่ใส่หน้ากาก",
-                              style: TextStyle(
-                                color: _outputs[0]["label"]=='0 mask'?Colors.green:Colors.deepOrangeAccent,
-                                fontSize: 28.0,
+        body: _loading
+            ? Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                color: Colors.grey[50],
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _image == null
+                        ? Container()
+                        : Image.file(_image,
+                            fit: BoxFit.contain,
+                            height: MediaQuery.of(context).size.height * 0.6),
+                    _outputs != null
+                        ? Column(
+                            children: <Widget>[
+                              Text(
+                                _outputs[0]["label"] == '0 mask'
+                                    ? "ใส่หน้ากาก"
+                                    : "ไม่ใส่หน้ากาก",
+                                style: TextStyle(
+                                  color: _outputs[0]["label"] == '0 mask'
+                                      ? Colors.green
+                                      : Colors.deepOrangeAccent,
+                                  fontSize: 28.0,
+                                ),
                               ),
-                            ),
-                          Text("มั่นใจ ${(_outputs[0]["confidence"]*100).round()}%",style: TextStyle(color:Colors.purpleAccent,fontSize:18),)
-                        ],
-                      )
-                      : Expanded(
-                        child: Container(
-
-                        )
-                      )
-                ],
-              ),
-            )
-    );
+                              Text(
+                                "มั่นใจ ${(_outputs[0]["confidence"] * 100).round()}%",
+                                style: TextStyle(
+                                    color: Colors.purpleAccent, fontSize: 18),
+                              )
+                            ],
+                          )
+                        : Expanded(child: Container())
+                  ],
+                ),
+              ));
   }
 }
